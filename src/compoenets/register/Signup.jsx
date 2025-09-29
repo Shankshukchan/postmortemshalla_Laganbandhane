4fb6032effa39f34fb55085f1333ec295e8f3fa5
shankshukchan <shankarspshukla@gmail.com>
2025-09-29 16:38:32 +0530

    .

--- src/compoenets/register/Signup.jsx
+++ src/compoenets/register/Signup.jsx
@@ -1,4 +1,5 @@
 import { useForm } from 'react-hook-form';
+import React from "react"
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import axios from 'axios';
